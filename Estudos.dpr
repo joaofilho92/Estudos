81673e22579f9e9d6d4585a04fcaca42d12dc3f5
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2024-05-25 22:09:56 +0200

    First Commit

---
+++ Estudos.dpr
@@ -0,0 +1,14 @@
+program Estudos;
+
+uses
+  Vcl.Forms,
+  uForm.Principal in 'Forms\uForm.Principal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
